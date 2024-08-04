bf1f8e649cc1cf4b93d82a0996c6fb18c94df717
minngaeng <minngaeng@gmail.com>
2024-08-04 12:55:22 +0900

    cutout 중심으로부터 잘린 부분 설정

--- components/DoughnutCard.tsx
+++ components/DoughnutCard.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+'use client';
+
+import { Chart } from 'chart.js';
+import { useEffect, useRef } from 'react';
 
 function DoughnutCard() {
-  return <div>DoughnutCard</div>;
+  const myChart = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    const chart = new Chart(myChart.current as HTMLCanvasElement, {
+      type: 'doughnut',
+      data: {
+        datasets: [
+          {
+            data: [10, 20, 30],
+            borderWidth: 0,
+          },
+        ],
+
+        labels: ['United States', 'Italy', 'Other'],
+      },
+      options: {
+        cutout: '80%',
+        plugins: {
+          legend: {
+            position: 'bottom',
+          },
+        },
+      },
+    });
+
+    return () => chart.destroy();
+  }, []);
+
+  return <canvas ref={myChart}></canvas>;
 }
 
 export default DoughnutCard;
